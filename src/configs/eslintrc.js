68113635d98b7be06f47a08667a9e5b4b2c80b06
Joel Voss <mail@joelvoss.com>
2020-06-10 20:54:18 +0200

    feat: remove eslint-plugin-jsx-a11y

--- src/configs/eslintrc.js
+++ src/configs/eslintrc.js
@@ -25,7 +25,6 @@ module.exports = {
 
   plugins: [
     'import',
-    ...(hasDep('react') ? ['jsx-a11y'] : []),
     ...(hasDep('react') ? ['react', 'react-hooks'] : []),
   ].filter(Boolean),
 
@@ -342,36 +341,6 @@ module.exports = {
           'react/style-prop-object': 'warn',
         }
       : {}),
-
-    // https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules
-    ...(hasDep('react')
-      ? {
-          'jsx-a11y/accessible-emoji': 'warn',
-          'jsx-a11y/alt-text': 'warn',
-          'jsx-a11y/anchor-has-content': 'warn',
-          'jsx-a11y/anchor-is-valid': [
-            'warn',
-            {
-              aspects: ['noHref', 'invalidHref'],
-            },
-          ],
-          'jsx-a11y/aria-activedescendant-has-tabindex': 'warn',
-          'jsx-a11y/aria-props': 'warn',
-          'jsx-a11y/aria-proptypes': 'warn',
-          'jsx-a11y/aria-role': 'warn',
-          'jsx-a11y/aria-unsupported-elements': 'warn',
-          'jsx-a11y/heading-has-content': 'warn',
-          'jsx-a11y/iframe-has-title': 'warn',
-          'jsx-a11y/img-redundant-alt': 'warn',
-          'jsx-a11y/no-access-key': 'warn',
-          'jsx-a11y/no-distracting-elements': 'warn',
-          'jsx-a11y/no-redundant-roles': 'warn',
-          'jsx-a11y/role-has-required-aria-props': 'warn',
-          'jsx-a11y/role-supports-aria-props': 'warn',
-          'jsx-a11y/scope': 'warn',
-        }
-      : {}),
-
     // https://github.com/facebook/react/tree/master/packages/eslint-plugin-react-hooks
     ...(hasDep('react')
       ? {
